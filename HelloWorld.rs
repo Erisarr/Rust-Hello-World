95e189c08a9409f97764ae2b2d83c5dca89f8de7
Eric Vargas <ericvargas2004@gmail.com>
2025-09-02 20:26:12 -0400

    Added HelloWorld rust file
    Uploaded a completed assignment 1

---
+++ HelloWorld.rs
@@ -0,0 +1,8 @@
+// Created by Eric Vargas
+// Followed closely from https://doc.rust-lang.org/rust-by-example/hello.html
+
+// The main function
+fn main() {
+    println!("Hello World!"); // macro to print
+    println!("I'm a Rustacean!"); // activity
+}
